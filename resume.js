a24f28c0d463c7b22be6c2dac910efb2a1a797e5
joseee177 <josezuniga1517@gmail.com>
2020-09-06 23:36:35 -0500

    fixed the about page issue

---
+++ resume.js
@@ -0,0 +1,11 @@
+function myanimation (){
+  document.getElementById("demo").innerHTML = "512-459-2222";
+}
+document.getElementById("atx").onclick = function(){
+  myfunction()
+};
+function myfunction(){
+  document.getElementById("atx").innerHTML=null;
+}
+function userclick(element){
+}
